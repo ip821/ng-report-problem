22e976fb0b626aee7ca51b21dbf3ea924da9d4e9
igor <igor>
2021-04-19 19:48:09 +0300

    Improving dialog.

--- src/app/report-problem/dialog.ts
+++ src/app/report-problem/dialog.ts
@@ -1,19 +1,33 @@
-import { Component, Inject } from "@angular/core";
+import { AfterViewInit, Component, Inject, OnInit, ViewChild } from "@angular/core";
 import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { MatStepper } from "@angular/material/stepper";
 import { ReportDialogData as ReportDialogData, ReprotDialogResult } from "./dialog-data";
 import { GeneralInfoFormGroup } from "./general-info.component";
 
 @Component({
   templateUrl: './dialog.html'
 })
-export class ReportDialogComponent {
+export class ReportDialogComponent implements OnInit, AfterViewInit {
 
   constructor(
     @Inject(MAT_DIALOG_DATA) private data: ReportDialogData
   ) {
     this.generalInfoForm = new GeneralInfoFormGroup();
   }
+  ngAfterViewInit(): void {
+    this.stepper.selectionChange.subscribe(it => {
+      if (it.selectedIndex === 2) {
+        setTimeout(() => this.inProgress = false, 2000);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  @ViewChild(MatStepper) stepper: MatStepper = null!;
 
   public result = new ReprotDialogResult();
   public generalInfoForm: GeneralInfoFormGroup;
+  public inProgress: boolean = true;
 }
